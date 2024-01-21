c6f5a9a7d9727709a545a4034c4d48ab2f85e9ee
Efe Yencilek <efefrk30@gmail.com>
2024-01-20 21:34:47 -0500

    A Dummy Setup to Run Subprocess and Extrapolate Data from File Explorer

---
+++ Subprocess.py
@@ -0,0 +1,25 @@
+# Subprocess class that opens FileExplorer and returns user input
+
+import subprocess as proc
+import time as t
+
+class Subprocess:
+
+    def __init__(self):
+        pass
+
+    def execute(self):
+        explorer_proc = proc.run(
+                            r'explorer "C:\Users\efefr.USER"'
+                            ,capture_output=True
+                            )
+        while True:
+            print(explorer_proc.args)
+            print(explorer_proc.returncode)
+            print(explorer_proc.stderr)
+            print(explorer_proc.stdout)
+            t.sleep(1)
+
+
+a = Subprocess()
+a.execute()
